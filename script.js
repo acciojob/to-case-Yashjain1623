7d21678881aa8b44584030193c37b3b0ee441907
Yashjain1623 <157748380+Yashjain1623@users.noreply.github.com>
2024-08-05 10:12:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function toCase(text) {
-  // write your code here
+function toCase(str) {
+  // Check if the input string is empty
+  if (str.length === 0) {
+    return '-';
+  }
+  
+  // Convert the string to lowercase and uppercase
+  const lowerCaseStr = str.toLowerCase();
+  const upperCaseStr = str.toUpperCase();
+  
+  // Return the formatted string with '-' as the delimiter
+  return `${lowerCaseStr}-${upperCaseStr}`;
 }
-
-// DO not change the code below
 
 const text = prompt("Enter text:");
 alert(toCase(text));
